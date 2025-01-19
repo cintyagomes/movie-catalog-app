9a9df169afec8a8c3ff3ce5ba61f7a6d44a19818
cintyagomes <cintya.gomes-ext@olxbr.com>
2025-01-19 10:40:42 -0300

    feat: Adds back button to the movie catalog

--- app/src/main/java/com/omdbapi/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/omdbapi/ui/MovieDetailFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
+import com.omdbapi.MainActivity
 import com.omdbapi.R
 import com.omdbapi.databinding.FragmentMovieDetailBinding
 import com.omdbapi.di.DaggerAppComponent
@@ -31,6 +34,17 @@ class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
             this,
             MovieViewModelFactory(repository)
         )[MovieViewModel::class.java]
+
+        binding.toolbar.apply {
+            (activity as? AppCompatActivity)?.setSupportActionBar(this)
+            (activity as? AppCompatActivity)?.supportActionBar?.setDisplayShowTitleEnabled(false)
+            setNavigationIcon(R.drawable.ic_arrow_back)
+
+            setNavigationOnClickListener {
+                val navController = (activity as? MainActivity)?.findNavController(R.id.nav_host_fragment)
+                navController?.navigateUp()
+            }
+        }
 
         return binding.root
     }
